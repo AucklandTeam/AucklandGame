d1f369453540d33652fcb7585937a3bb988ad37a
maksim.rameev@x5.ru <maksim.rameev@x5.ru>
2022-04-07 16:37:50 +0300

    add canvas

--- src/components/pages/Game/conponents/CanvasComponent.jsx
+++ src/components/pages/Game/conponents/CanvasComponent.jsx
@@ -16,9 +16,9 @@ const CanvasComponent = () => {
     const bg = new Image();
     const debris = new Image();
     // константы
-    const angleIncValue = 10;
-    const speedIncValue = 1;
-    const speedInertion = 0.95;
+    const angleIncValue = 5;
+    const speedIncValue = 0.5;
+    const speedInertion = 0.96;
     const shipWith = 100;
     const shipHeight = 124;
     const bulletRadius = 3;
@@ -112,49 +112,45 @@ const CanvasComponent = () => {
     };
 
     const updateScene = () => {
-        if (isLoaded) {
-            // очищаем весь канвас перед перерисовкой
-            ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
-            // рисуем объект в новых кординатах
-            // фон
-            ctx.drawImage(bg, 0, 0);
-            // фон метеориты
-            ctx.drawImage(debris, debrisX, debrisY);
-            // пули
-            bullets = bullets.filter(el => el.getVisible());
-            bullets.forEach(el => {
-                ctx.beginPath();
-                ctx.arc(el.getPos().x, el.getPos().y, bulletRadius, 0, 2 * Math.PI);
-                ctx.fillStyle = 'orange';
-                ctx.fill();
-                el.update();
-                ctx.closePath();
-            })
-            // корабль
-            // сохраняем канвас
-            ctx.save();
-            // переносим центр в центр корабля
-            ctx.translate(xMove + shipWith/2, yMove + shipHeight/2);
-            // поворачиваем корабль
-            ctx.rotate(Math.PI/180 * angle);
-            // переносим центр обратно
-            ctx.translate(-xMove - shipWith/2, -yMove - shipHeight/2);
-            // отрисовываем корабль
-            ctx.drawImage(spaceship, xMove, yMove);
-            // восстанавливаем канвас
-            ctx.restore();
-        }
+
+        // очищаем весь канвас перед перерисовкой
+        ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
+        // рисуем объект в новых кординатах
+        // фон
+        ctx.drawImage(bg, 0, 0);
+        // фон метеориты
+        ctx.drawImage(debris, debrisX, debrisY);
+        // пули
+        bullets = bullets.filter(el => el.getVisible());
+        bullets.forEach(el => {
+            ctx.beginPath();
+            ctx.arc(el.getPos().x, el.getPos().y, bulletRadius, 0, 2 * Math.PI);
+            ctx.fillStyle = 'orange';
+            ctx.fill();
+            el.update();
+            ctx.closePath();
+        })
+        // корабль
+        // сохраняем канвас
+        ctx.save();
+        // переносим центр в центр корабля
+        ctx.translate(xMove + shipWith/2, yMove + shipHeight/2);
+        // поворачиваем корабль
+        ctx.rotate(Math.PI/180 * angle);
+        // переносим центр обратно
+        ctx.translate(-xMove - shipWith/2, -yMove - shipHeight/2);
+        // отрисовываем корабль
+        ctx.drawImage(spaceship, xMove, yMove);
+        // восстанавливаем канвас
+        ctx.restore();
+
     }
 
     const loop = (time) => {
-        if (isLoaded) {
-            // двигаем метеориты
-            debrisX = (debrisX + 1) % canvas.current.width;
-            updateShip(time);
-            updateScene();
-        }
-
-
+        // двигаем метеориты
+        debrisX = (debrisX + 1) % canvas.current.width;
+        updateShip(time);
+        updateScene();
         requestRef.current = requestAnimationFrame(loop);
     };
 
