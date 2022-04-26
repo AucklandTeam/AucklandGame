6fcb1d70d39bacc3d6956e72cc7d6acc01ea4c20
maksim.rameev <maksim.rameev@x5.ru>
2022-04-26 14:36:16 +0300

    ASTER026: service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,8 +1,47 @@
-self.addEventListener('install', async event => {
-    console.log('install');
+const CACHE_NAME = 'auckland-game-cache-v1';
 
+const URLS = [
+    '/',
+    '/game',
+    '/forum',
+    '/sign-up',
+    '/bundle.js',
+];
+
+self.addEventListener('install', (event) => {
+    console.log('install');
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(URLS);
+            })
+            .catch((err) => {
+                console.log(err);
+                throw err;
+            }),
+    );
 });
 
-self.addEventListener('activate', e => {
-    console.log('activate');
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request);
+        }),
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches
+            .keys()
+            .then((cacheNames) =>
+                Promise.all(
+                    cacheNames
+                        .filter(() => true)
+                        .map((name) => caches.delete(name)),
+                ),
+            ),
+    );
 });
