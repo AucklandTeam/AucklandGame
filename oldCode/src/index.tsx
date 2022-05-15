a78243a2d660a55095382fedfbafb8fcfd0abd3d
maksim.rameev <maksim.rameev@x5.ru>
2022-05-15 16:50:10 +0300

    new ssr

---
+++ oldCode/src/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'src/components/App/App';
+import 'oldCode/src/assets/base.scss';
+import {Provider, TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
+import createSagaMiddleware from '@redux-saga/core';
+import configureStore from './core/store';
+import {rootSaga} from './core/Saga';
+
+const sagaMiddleWare = createSagaMiddleware();
+
+const store = configureStore(sagaMiddleWare);
+sagaMiddleWare.run(rootSaga);
+
+export type AppState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch();
+export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+            console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
+
+ReactDOM.hydrate(<Provider store={store}><App /></Provider>, document.querySelector('#root'));
