535324656164f8d91b6a624251ab0adb1b82d7d0
ManuTeMaia <cath@manutemaia.com>
2022-06-25 19:10:24 +0300

    ASTER043 fix: change connect fn to accept process env
    Signed-off-by: ManuTeMaia <cath@manutemaia.com>

--- src/server/db/connect.ts
+++ src/server/db/connect.ts
@@ -5,27 +5,16 @@ import TopicCategory from 'server/Model/topicCategory'
 import Comment from 'server/Model/comment'
 import Reply from 'server/Model/reply'
 
+export async function dbConnect(sequelizeOptions: SequelizeOptions ) {
+    try {
 
-// @ts-ignore
-export const sequelizeOptions: SequelizeOptions = {
-	host: 'c-c9qrj8eshi2b3tho0r6m.rw.mdb.yandexcloud.net',
-	port: 6432,
-	username: process.env.POSTGRES_USER,
-	password: process.env.POSTGRES_PASSWORD,
-	database: process.env.POSTGRES_DB,
-	dialect: 'postgres'
-}
+        const sequelize = new Sequelize(sequelizeOptions)
 
-export const sequelize = new Sequelize(sequelizeOptions)
-
-sequelize.addModels([User, Topic, TopicCategory, Comment, Reply])
-
-export async function dbConnect() {
-	try {
-		await sequelize.authenticate()
-		await sequelize.sync({ force: true })
-		console.log('Connection has been established successfully.')
-	} catch (e) {
-		console.error('Unable to connect to the database:', e)
-	}
+        sequelize.addModels([User, Topic, TopicCategory, Comment, Reply])
+        await sequelize.authenticate()
+        await sequelize.sync({ force: true })
+        console.log('Connection has been established successfully.')
+    } catch (e) {
+        console.error('Unable to connect to the database:', e)
+    }
 }
