a0f09bc37f06a4c0899b14b13a95a113d0b3ab4c
ManuTeMaia <cath@manutemaia.com>
2022-06-28 15:16:04 +0300

    ASTER045 fix: service-worker.js
    Signed-off-by: ManuTeMaia <cath@manutemaia.com>

--- src/service-worker.js
+++ src/service-worker.js
@@ -25,7 +25,6 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', function (event) {
 	event.respondWith(
 		fetch(event.request).catch(function () {
-			console.log('All fetched')
 			return caches.match(event.request)
 		})
 	)
